bfa28069df1c67a5dd05cd10bac0e46e214dbfd8
Guilherme Barbosa <guilhermebarbosa.ext@CRESSEM.INTRA>
2024-10-23 11:24:08 -0300

    settings

--- core/settings.py
+++ core/settings.py
@@ -175,7 +175,7 @@ USE_TZ = False
 # STATICFILES_DIRS = [BASE_DIR, 'static']
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/guianderson/safe-instruments/static'  # Diretório onde os arquivos estáticos coletados serão armazenados
+STATIC_ROOT = '/home/guianderson/safe-instruments/static/third_party/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',  # Diretórios adicionais onde o Django procurará por arquivos estáticos
 ]
